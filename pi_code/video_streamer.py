c41bc71986fd6e20c39cc8ff811f68fad4827f32
shutchison <scott.allen.hutchison@gmail.com>
2021-11-06 12:01:03 -0500

    working video streaming to remote server

--- pi_code/video_streamer.py
+++ pi_code/video_streamer.py
@@ -1,95 +1,45 @@
-# Web streaming example
-# Source code from the official PiCamera package
-# http://picamera.readthedocs.io/en/latest/recipes2.html#web-streaming
-
 import io
+import socket
+import struct
+import time
 import picamera
-import logging
-import socketserver
-from threading import Condition
-from http import server
 
-PAGE="""\
-<html>
-<head>
-<title>Raspberry Pi - Surveillance Camera</title>
-</head>
-<body>
-<center><h1>Raspberry Pi - Surveillance Camera</h1></center>
-<center><img src="stream.mjpg" width="640" height="480"></center>
-</body>
-</html>
-"""
+# Connect a client socket to my_server:8000 (change my_server to the
+# hostname of your server)
+client_socket = socket.socket()
+client_socket.connect(('', 8000))
 
-class StreamingOutput(object):
-    def __init__(self):
-        self.frame = None
-        self.buffer = io.BytesIO()
-        self.condition = Condition()
+# Make a file-like object out of the connection
+connection = client_socket.makefile('wb')
+try:
+    camera = picamera.PiCamera()
+    camera.resolution = (640, 480)
+    # Start a preview and let the camera warm up for 2 seconds
+    camera.start_preview()
+    time.sleep(2)
 
-    def write(self, buf):
-        if buf.startswith(b'\xff\xd8'):
-            # New frame, copy the existing buffer's content and notify all
-            # clients it's available
-            self.buffer.truncate()
-            with self.condition:
-                self.frame = self.buffer.getvalue()
-                self.condition.notify_all()
-            self.buffer.seek(0)
-        return self.buffer.write(buf)
-
-class StreamingHandler(server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(301)
-            self.send_header('Location', '/index.html')
-            self.end_headers()
-        elif self.path == '/index.html':
-            content = PAGE.encode('utf-8')
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
-            self.end_headers()
-            self.wfile.write(content)
-        elif self.path == '/stream.mjpg':
-            self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with output.condition:
-                        output.condition.wait()
-                        frame = output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
-                    self.end_headers()
-                    self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
-        else:
-            self.send_error(404)
-            self.end_headers()
-
-class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
-    allow_reuse_address = True
-    daemon_threads = True
-
-with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
-    output = StreamingOutput()
-    #Uncomment the next line to change your Pi's Camera rotation (in degrees)
-    #camera.rotation = 90
-    camera.start_recording(output, format='mjpeg')
-    try:
-        address = ('', 8000)
-        server = StreamingServer(address, StreamingHandler)
-        server.serve_forever()
-    finally:
-        camera.stop_recording()
-
+    # Note the start time and construct a stream to hold image data
+    # temporarily (we could write it directly to connection but in this
+    # case we want to find out the size of each capture first to keep
+    # our protocol simple)
+    start = time.time()
+    stream = io.BytesIO()
+    for foo in camera.capture_continuous(stream, 'jpeg'):
+        # Write the length of the capture to the stream and flush to
+        # ensure it actually gets sent
+        connection.write(struct.pack('<L', stream.tell()))
+        connection.flush()
+        # Rewind the stream and send the image data over the wire
+        stream.seek(0)
+        connection.write(stream.read())
+        # If we've been capturing for more than 30 seconds, quit
+        if time.time() - start > 30:
+            break
+        # Reset the stream for the next capture
+        stream.seek(0)
+        stream.truncate()
+    # Write a length of zero to the stream to signal we're done
+    connection.write(struct.pack('<L', 0))
+finally:
+    connection.close()
+    client_socket.close()
